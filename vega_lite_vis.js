7edbeee43c45e1c444ac0de8fea4f5824618b42f
zepengtan <113353950+zepengtan@users.noreply.github.com>
2022-10-07 12:17:18 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,21 @@
+var vg_1 = "map.vg.json";
+var vg_2 = "multiple-bar.vg.json";
+var vg_3 = "slope.vg.json"
+var vg_4 = "bump.vg.json"
+
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bar", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#slope", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bump", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
